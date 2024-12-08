cb468140501d82276f141234abb34e485b68ee99
Ben Timby <btimby@gmail.com>
2024-12-08 14:09:39 -0500

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # vite-plugin-svg-template
 Import SVG as template src.
+
+This plugin allows you to use an external SVG file as a component's template.
+
+The main benefits of this approach over other svg plugins or methods are:
+
+ - You can edit the .svg file with a graphical editor, like Inkscape.
+ - You can use placeholders and other template language elements like `{{ replaceMe }}`.
+
+This plugin handles any embedded styles within the .svg file by scoping them to the `<svg>` tag. You can apply additional styles in the component's style section.
+
+## Example
+
+vite.config.js:
+```javascript
+import { defineConfig } from 'vite'
+import vueSvgTemplate from 'vite-plugin-svg-template';
+import vue from '@vitejs/plugin-vue'
+
+// https://vite.dev/config/
+export default defineConfig({
+    plugins: [
+        vueSvgTemplate(),
+        vue(),
+    ],
+    ...
+});
+```
+
+Now in your single-file components, you can use an external SVG file as a template.
+
+My-Component.vue:
+```html
+<template src="./My-Component.svg"></template>
+
+<script>
+export default {
+    name: 'My-Component',
+
+    ...
+};
+</script>
+
+<style scoped>
+/* You can define additional styles here */
+</style>
+```
---
+++ src/index.js
@@ -0,0 +1,85 @@
+import { readFileSync } from 'fs';
+import HTMLParser from 'node-html-parser';
+import { compileTemplate } from '@vue/compiler-sfc';
+
+const STYLE = /<style[^>]*>([\s\S]*)<\/style>/mig
+const SCOPE_ATTR = 'data-id-css-scope';
+
+function resolve(id) {
+    const [idWithoutQuery, query] = id.split('?', 2);
+    const params = Object.fromEntries(new URLSearchParams(query));
+    const matched = (
+        idWithoutQuery.endsWith('.svg') &&
+        params.type === 'template'
+    );
+  
+    return {
+        idWithoutQuery,
+        params,
+        matched,
+    };
+}
+  
+function svgTemplatePlugin(options = {}) {
+    const compilerOptions = {};
+
+    if (options.stripTags) {
+        compilerOptions.isCustomElement = (name) => {
+            return options.stripTags.includes(name);
+        };
+    }
+
+    return {
+        name: 'svg-template',
+
+/*        
+        resolveId() {
+            console.log('resolveId:', arguments);
+        },
+
+        load() {
+            console.log('load:', arguments);
+        },
+*/
+
+        async transform(source, id) {
+            const { idWithoutQuery, params, matched } = resolve(id);
+
+            if (!matched) {
+                return;
+            }
+
+            // console.log(id);
+            source = readFileSync(idWithoutQuery, 'utf8');
+
+            const doc = HTMLParser.parse(source);
+            const svg = doc.querySelector('svg');
+            const style = doc.querySelector('defs > style');
+
+            if (svg && style) {
+                const idStr = makeId(8);
+                svg.setAttribute(SCOPE_ATTR, idStr);
+                style.set_content(`svg[${SCOPE_ATTR}="${idStr}"] {\n${style.rawText}\n}`);
+                source = svn.toString();
+            }
+
+            const { code: render, map } = compileTemplate({
+                id,
+                filename: id,
+                source,
+                transformAssetUrls: false,
+                compilerOptions,
+            });
+
+            const code = `${render}\nexport default { render };`;
+            // console.log(code);
+
+            return {
+                code,
+                map,
+            };
+        },
+    };
+}
+
+export default svgTemplatePlugin;
